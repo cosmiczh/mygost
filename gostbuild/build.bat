31d271d7879e864e1d3a8db9eed45cb1264a0b39
cosmiczh <andrew.zhang@sina.com>
2022-10-26 12:53:05 +0800

    chg：改名移位readme.docx文件

--- gostbuild/build.bat
+++
@@ -1,9 +0,0 @@
-set GOOS=windows
-set GOARCH=amd64
-go build -o gost-win-x64.exe ../cmd/gost
-
-set GOOS=linux
-go build -o gost-linux-x64 ../cmd/gost
-set GOARCH=arm
-go build -o gost-linux-arm ../cmd/gost
-
